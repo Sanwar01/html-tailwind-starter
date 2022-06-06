b0e5e66525f9a4077c74db647c3208ba210cc5ba
Sanwar10 <sulemananwar99@gmail.com>
2022-06-06 22:20:31 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['src/*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
